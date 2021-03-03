5d77fdb204d37b80c261a7bf46956f2862ee6656
Moreshwar Nabar <mrnabar@gmail.com>
2021-03-03 19:21:23 +0530

    commit to branch x

---
+++ src/com/demo/Test2.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+public class Test2 {
+	
+	public static void main(String[] args) {
+		String hungry = "no";
+		
+		if (hungry == "yes") 
+			System.out.println("Eat something...");
+		else
+			System.out.println("do work...");
+	}
+	
+}
